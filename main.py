bdcd3140eee5a909aab5724d5e0428a82323f22f
alexandra.lidman@wideops.com <alexandra.lidman@wideops.com>
2022-12-14 16:38:45 +0200

    testapp

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return "Считайте себя сумасшедшим, и никогда не ходите на работу."
+   return "Подумайте что вы умный человек. "
 
 
 if __name__ == '__main__':
